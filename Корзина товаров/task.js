9846db2b15117f4d5b0554402ae35aa11662d982
saff84 <saff84@mail.ru>
2023-02-10 20:33:21 +0300

    all done

--- Корзина товаров/task.js
+++ Корзина товаров/task.js
@@ -1,20 +1,48 @@
 const addToCard = document.querySelectorAll(".product__add"),
-card = document.querySelector(".cart__products"),
-products = document.querySelectorAll(".product"),
-quanty = document.querySelectorAll(".product__quantity-control")
-
-
+    cards = document.querySelector(".cart__products"),
+    products = document.querySelectorAll(".product"),
+    quanty = document.querySelectorAll(".product__quantity-control")
 
 
 addToCard.forEach((el, index) => {
+
     el.addEventListener("click", () => {
-        squ = document.createElement("div")
-        squ.classList.add("cart__product")
-        squ.innerHTML = `<img class="cart__product-image" src=${products[index].querySelector(".product__image").src}>
-        <div class="cart__product-count">${products[index].querySelector(".product__quantity-value").textContent}</div>`
-        squ.setAttribute("data-id", `${products[index].getAttribute("data-id")}`)
-        card.append(squ)
-    } )
+
+        funcAddToCard(el, index)
+    })
 })
 
+quanty.forEach(el => {
+    el.addEventListener("click", () => {
+        let parent = el.closest(".product__quantity-controls")
+        quant = parent.querySelector(".product__quantity-value")
+        if (el.classList.contains("product__quantity-control_dec") && Number(quant.textContent) <= 1) return
+        else if (el.classList.contains("product__quantity-control_dec")) {
+            quant.textContent = Number(quant.textContent) - 1
+        }
+        else quant.textContent = Number(quant.textContent) + 1
 
+    })
+})
+
+function funcAddToCard(el, index) {
+    let cart = el.closest(".product"),
+        productInCart = document.querySelectorAll(".cart__product")
+    if (productInCart.length > 0) {//Добавление в карточку товара уже есть в корзине
+        for (let i = 0; i < productInCart.length; i++) {
+            if (productInCart[i].getAttribute("data-id") === cart.getAttribute("data-id")) {
+                productInCart[i].querySelector(".cart__product-count").textContent = Number(productInCart[i].querySelector(".cart__product-count").textContent) + Number(products[index].querySelector(".product__quantity-value").textContent)
+                return
+            }
+        }
+    }
+    
+    squ = document.createElement("div")//Первичное создание карточки в корзине
+    squ.classList.add("cart__product")
+    squ.innerHTML = `<img class="cart__product-image" src=${products[index].querySelector(".product__image").src}>
+                <div class="cart__product-count">${products[index].querySelector(".product__quantity-value").textContent}</div>`
+    squ.setAttribute("data-id", `${products[index].getAttribute("data-id")}`)
+    cards.append(squ)
+
+    return
+}

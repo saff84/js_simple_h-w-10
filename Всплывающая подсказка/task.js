3304a7a94b4d93cfdc42af90d0fb7f1734a5b987
saff84 <saff84@mail.ru>
2023-02-13 19:54:13 +0300

    task 1 fixed

--- Всплывающая подсказка/task.js
+++ Всплывающая подсказка/task.js
@@ -1,32 +1,36 @@
+
+
 document.addEventListener("click", e => {  //слушаем клики по документу
 
     if (e.target.classList.contains('has-tooltip') || e.target.closest('.has-tooltip')) { //проверям клик нужный элемент или рядом с ним
-        tooltipShow(e.target)                   //показываем посказку
+        tooltipShow(e.target)                 //показываем посказку
         e.preventDefault()
-    }
-    else {
-        tooltipHide()                           //иначе прячем если она есть
     }
 })
 
 function tooltipShow(link) {
+
     if (!document.querySelector('.tooltip')) { //если еще нет div с подсказкой то создаем
         tooltipCreate(link);
     }
-    let tooltip = document.querySelector('.tooltip'),  // получаем созданный div подсказки
-        coord = link.getBoundingClientRect();               // получаем координаты блока для подсказки
-    tooltip.innerHTML = link.getAttribute('title');         //обновляем данные для подсказки
-    tooltip.classList.toggle('tooltip_active');
-    tooltip.style.left = coord.left + "px";
-    tooltip.style.top = coord.bottom + "px";
 
+    let tooltip = document.querySelector('.tooltip'),  // получаем существующий div подсказки
+        coord = link.getBoundingClientRect();               // получаем координаты блока для подсказки
+
+    if (tooltip.textContent === link.getAttribute("title")) { // сравниваем открытую подсказку и клик
+        tooltipHide(tooltip)
+    } else {
+        tooltipHide(tooltip)                                    // закрываем преыдущюю подсказку
+        tooltip.innerHTML = link.getAttribute('title');         //обновляем данные для новой подсказки
+        tooltip.classList.add('tooltip_active');
+        tooltip.style.left = coord.left + "px";
+        tooltip.style.top = coord.bottom + "px";
+    }
 }
 
-function tooltipHide() {
-    let tooltip = document.querySelector('.tooltip');
-    if (tooltip) {
-        tooltip.classList.remove('tooltip_active');
-    }
+function tooltipHide(link) {
+    link.classList.remove('tooltip_active');
+    link.textContent = ""
 }
 
 function tooltipCreate() {
